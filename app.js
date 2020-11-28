0e739a3b74135c4a41fc2df741be8dddf599214e
Jovan Zivlak <jovanzivlak@Jovans-MacBook-Air.local>
2020-11-28 19:35:53 +0100

    structure organization

--- app.js
+++ app.js
@@ -27,54 +27,55 @@ $(document).ready(() => {
     $('#language')[0].innerText = val;
   };
 
-  var playing = 0;
+  // var playing = 0;
 
-  anyAudioPlaying = (id) => {
-    if (playing != 0 && playing != id) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+  // anyAudioPlaying = (id) => {
+  //   if (playing != 0 && playing != id) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // };
 
-  var play = 'fa-play-circle';
-  var pause = 'fa-pause-circle';
-  var color = 'audio-playing';
+  // var play = 'fa-play-circle';
+  // var pause = 'fa-pause-circle';
+  // var color = 'audio-playing';
 
-  stopAudio = (id) => {
-    var audio = document.getElementById(id);
-    var element = document.getElementById(`poem${id}`);
-    element.classList.remove(pause, color);
-    element.classList.add(play);
-    audio.pause();
-    audio.currentTime = 0;
-    playing = 0;
-  };
+  // stopAudio = (id) => {
+  //   var audio = document.getElementById(id);
+  //   var element = document.getElementById(`poem${id}`);
+  //   element.classList.remove(pause, color);
+  //   element.classList.add(play);
+  //   audio.pause();
+  //   audio.currentTime = 0;
+  //   playing = 0;
+  // };
 
-  playAudio = (id) => {
-    if (anyAudioPlaying(id)) {
-      stopAudio(playing);
-    }
-    var element = document.getElementById(`poem${id}`);
-    if (element.className.includes(play)) {
-      var audio = document.getElementById(id);
-      element.classList.remove(play);
-      element.classList.add(pause, color);
-      audio.play();
-      playing = id;
-    } else {
-      stopAudio(id);
-    }
-  };
+  // playAudio = (id) => {
+  //   if (anyAudioPlaying(id)) {
+  //     stopAudio(playing);
+  //   }
 
-  poemsAudioToIdMapper = (id) => {
-    switch (id) {
-      case 1:
-        return 'huk_mrtve_sove';
-      default:
-        break;
-    }
-  };
+  //   var element = document.getElementById(`poem${id}`);
+  //   if (element.className.includes(play)) {
+  //     var audio = document.getElementById(id);
+  //     element.classList.remove(play);
+  //     element.classList.add(pause, color);
+  //     audio.play();
+  //     playing = id;
+  //   } else {
+  //     stopAudio(id);
+  //   }
+  // };
+
+  // poemsAudioToIdMapper = (id) => {
+  //   switch (id) {
+  //     case 1:
+  //       return 'huk_mrtve_sove';
+  //     default:
+  //       break;
+  //   }
+  // };
 
   toggleDisplay = (id) => {
     var x = document.getElementById(id);
@@ -85,3 +86,4 @@ $(document).ready(() => {
     }
   };
 });
+
---
+++ js/en.js
@@ -0,0 +1,9 @@
+(function () {
+  $('#includeBiography').load('./pages/en/biography.html');
+  $('#includePoetry').load('./pages/sr/poetry.html');
+  $('#includeBooks').load('./pages/sr/books.html');
+  $('#includeInterview').load('./pages/sr/interview.html');
+  $('#includeVideo').load('./pages/video.html');
+  $('#includeFooter').load('./pages/footer.html');
+  $('#includeAudio').load('./pages/sr/audio.html');
+})();
