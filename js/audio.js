c20161fcba9f383bfcbb5971ed5907861fe5782a
Jovan Zivlak <zivlakjovan@gmail.com>
2020-12-13 17:47:39 +0100

    poems

--- js/audio.js
+++ js/audio.js
@@ -40,38 +40,76 @@
     }
   };
 
-  poemsAudioToIdMapper = (id) => {
-    switch (id) {
-      case 1:
-        return 'huk_mrtve_sove';
-      default:
-        break;
-    }
-  };
+  // poemsAudioToIdMapper = (id) => {
+  //   switch (id) {
+  //     case 1:
+  //       return 'huk_mrtve_sove';
+  //     default:
+  //       break;
+  //   }
+  // };
 
   const publicationIds = [
-    
+    'brodar',
     'skola',
     'cestar',
-    'brodar',
+    'tronozac',
     'cekrk',
     'napev',
     'izvestaj',
-    'cegrtus',
+    'cegrtusa',
+    'ostrvo',
+    'gajde',
+    'nove',
   ];
+
+  const sr_brodar = [
+    { name: 'Благост линија', link: 'blagost_linija' },
+    { name: 'Ноћне звери', link: 'nocne_zveri' },
+  ];
+
+  const sr_skola = [];
+  const sr_cestar = [];
+  const sr_tronozac = [
+    { name: 'Радосно за догађај везан', link: 'radosno_za_dogadjaj_vezan' },
+  ];
+  const sr_cekrk = [
+    { name: 'Осветлјенје је слабо', link: 'osvetljenje_je_slabo' },
+    { name: 'Узлеће шишмиш', link: 'uzlece_sismis' },
+  ];
+  const sr_napev = [{ name: 'Зимски извештај', link: 'zimski_izvestaj' }];
+  const sr_izvestaj = [];
+  const sr_cegrtusa = [];
+  const sr_ostrvo = [
+    { name: 'Поводац', link: 'povodac' },
+    { name: 'Острво', link: 'ostrvo' },
+  ];
+  const sr_gajde = [{ name: 'Гвоздене вилице', link: 'gvozdene_vilice' }];
+  const sr_nove = [{ name: 'Поседи', link: 'posedi' }];
+
+  const srpoems = new Object();
+  srpoems['brodar'] = sr_brodar;
+  srpoems['skola'] = sr_skola;
+  srpoems['cestar'] = sr_cestar;
+  srpoems['tronozac'] = sr_tronozac;
+  srpoems['cekrk'] = sr_cekrk;
+  srpoems['napev'] = sr_napev;
+  srpoems['izvestaj'] = sr_izvestaj;
+  srpoems['cegrtusa'] = sr_cegrtusa;
+  srpoems['ostrvo'] = sr_ostrvo;
+  srpoems['gajde'] = sr_gajde;
+  srpoems['nove'] = sr_nove;
 
   const classClosed = 'fa-caret-right';
   const classOpen = 'fa-caret-down';
 
   const closeElement = (element, id) => {
-    if (element.childElementCount > 0) {
       const icon = document.getElementById(`${id}icon`);
       if (icon) {
         icon.classList.remove(classOpen);
         icon.classList.add(classClosed);
       }
       element.innerHTML = '';
-    }
   };
 
   const closeMulty = (ids) => {
@@ -89,17 +127,22 @@
       closeMulty(publicationIds.filter((x) => x !== id));
     }
 
-    if (el && el.childElementCount > 0) {
+    const icon = document.getElementById(`${id}icon`);
+    if (icon.classList.contains(classOpen)) {
       closeElement(el, id);
-    } else if (el) {
-      const icon = document.getElementById(`${id}icon`);
+    } else {
       if (icon) {
         icon.classList.remove(classClosed);
         icon.classList.add(classOpen);
       }
-      el.innerHTML += `<div onclick=loadPoem("huk_mrtve_sove")>Huk mrtve sove</div>`;
-      el.innerHTML += `<div onclick=loadPoem("uzlece_sismis")>Uzlece sismis</div>`;
-      el.innerHTML += `<div onclick=loadPoem("huk_mrtve_sove")>Huk Mrtve sove</div>`;
+      const poems = srpoems[id];
+      poems.forEach((p) => {
+        el.innerHTML += `<div onclick=loadPoem("${p.link}")>${p.name}</div>`;
+      });
+
+      // el.innerHTML += `<div onclick=loadPoem("huk_mrtve_sove")>Huk mrtve sove</div>`;
+      // el.innerHTML += `<div onclick=loadPoem("uzlece_sismis")>Uzlece sismis</div>`;
+      // el.innerHTML += `<div onclick=loadPoem("huk_mrtve_sove")>Huk Mrtve sove</div>`;
     }
   };
 
