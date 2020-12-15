79c01ecad0a94fd74ebb68f8d29a8c34053ddfb4
Jovan Zivlak <zivlakjovan@gmail.com>
2020-12-15 22:11:42 +0100

    events sr

---
+++ js/events.js
@@ -0,0 +1,9 @@
+(function () {
+  download = () => {
+    $('download').attr({
+      target: '_blank',
+      href:
+        'http://127.0.0.1:5501/pages/sr/events/pdf/Katalog_XV_MNKF%20%20%2011_2020.pdf',
+    });
+  };
+})();
